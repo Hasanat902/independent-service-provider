2e4191706ac8ac85b1355014a01ba053d0d94c82
Hasanat902 <abulhasanat902@gmail.com>
2022-04-17 22:09:26 +0600

    customer comment added

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Comment.css";
 
-const Comment = () => {
-    return (
+const Comment = ({ comment }) => {
+  const { img, name, describe } = comment;
+
+  return (
+    
+      <div className="col-12 col-md-6 col-lg-4 shadow p-3">
+        <img className="customer-img me-3" src={img} alt="" />
         <div>
-            
+          <h5 className="my-3">{name}</h5>
+          <p>{describe}</p>
         </div>
-    );
+      </div>
+  );
 };
 
 export default Comment;
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,10 +2,24 @@ import React from 'react';
 import customer1 from '../../images/customer/user1.jpg';
 import customer2 from '../../images/customer/user2.jpg';
 import customer3 from '../../images/customer/user3.jpg';
+import Comment from '../Comment/Comment';
 
 const Comments = () => {
+
+    const comments = [
+        {id: 1, img: customer1, name: "Nathan Smith", describe: "This is one of the best homemade food catering service.You guys can order food from them for your office and home."},
+        {id: 2, img: customer2, name: "Jason Holder", describe: "I tried so many homemade food but their food taste is so unique and good. I recommend their food"},
+        {id: 3, img: customer3, name: "Steve Waugh", describe: "One of the best homemade food ever.And their food is fresh and hygienic.You can order from them"}
+    ]
+
     return (
-        <div>
+        <div className='container'>
+            <h2 className='text-center my-5'>What Our Customer Say?</h2>
+            <div className="row">
+                {
+                    comments.map(comment => <Comment key={comment.id} comment={comment}></Comment>)
+                }
+            </div>
             
         </div>
     );
