deaf029d0645d83ac422e3bd1225ad9515791028
Hasanat902 <abulhasanat902@gmail.com>
2022-04-17 16:41:23 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
