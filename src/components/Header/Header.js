821a604f3dda3886c86f35aecf33371b8331aa8e
Hasanat902 <abulhasanat902@gmail.com>
2022-04-17 23:58:24 +0600

    signout system added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
+
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  }
+
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
@@ -18,9 +28,14 @@ const Header = () => {
               <Nav.Link href="home#services">Services</Nav.Link>
               <Nav.Link as={CustomLink} to="/blogs">Blogs</Nav.Link>
               <Nav.Link as={CustomLink} to="/about">About Us</Nav.Link>
-              <Nav.Link as={CustomLink  } to="/login">
+              {
+                user ?
+                <button onClick={handleSignOut} className="btn btn-link text-decoration-none text-white">SignOut</button>
+                :
+                <Nav.Link as={CustomLink  } to="/login">
                 SignIn
               </Nav.Link>
+              }
             </Nav>
           </Navbar.Collapse>
         </Container>
